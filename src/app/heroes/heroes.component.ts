a753190a956e9d2ee1bb8f2aeb9382064bde0661
EngiBeerHub <engibeer0422@gmail.com>
2023-05-07 15:02:44 +0900

    add dashboard and go back button

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,14 +9,8 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
 
   heroes: Hero[] = [];
-
-  selectedHero?: Hero;
 
   constructor(
     private heroService: HeroService,
@@ -25,11 +19,6 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
     this.getHeros();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeros(): void {
